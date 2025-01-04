1bc16ac6ef471afe49166b1bb99962ab471996af
Trent Nelson <trent@trent.me>
2025-01-03 17:44:36 -0800

    feat: Add useRef import to Wiki component for future enhancements

--- src/Wiki.jsx
+++ src/Wiki.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { Container, Form, FormControl, ListGroup, Card } from "react-bootstrap";
 
 const Wiki = () => {
