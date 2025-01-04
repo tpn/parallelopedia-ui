1309918e4ff7d7ec5e69679580ff2731cb931ef5
Trent Nelson <trent@trent.me>
2025-01-03 18:49:57 -0800

    feat: Add Utils component for utility functions

---
+++ src/Utils.jsx
@@ -0,0 +1,9 @@
+function bytesToHuman(bytes) {
+  if (bytes === 0) return "0 B";
+
+  const units = ["B", "KB", "MB", "GB", "TB", "PB", "EB"];
+  const k = 1024;
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+  return `${(bytes / Math.pow(k, i)).toFixed(1)} ${units[i]}`;
+}
