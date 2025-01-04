938cf7bdb44b3c591f329bdce0f613f5f7673543
Trent Nelson <trent@trent.me>
2025-01-03 17:01:15 -0800

    feat: Add main application component in App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Stats from "./Stats";
+import Container from "react-bootstrap/Container";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.scss";
+import { Tabs } from "react-bootstrap";
+import Tab from "react-bootstrap/Tab";
+
+const App = () => {
+  return (
+    <Container>
+      <header className="py-3">
+        <h1>Parallelopedia UI</h1>
+      </header>
+      <main>
+        <Tabs defaultActiveKey="stats" id="main-tabs">
+          <Tab title="Wiki" eventKey="wiki"></Tab>
+          <Tab title="Stats" eventKey="stats">
+            <Stats />
+          </Tab>
+        </Tabs>
+      </main>
+    </Container>
+  );
+};
+
+export default App;
