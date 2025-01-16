b5c5c72f046eaa7155cdee8a34627e34dad221d4
Trent Nelson <trent@trent.me>
2025-01-16 14:58:03 -0800

    fix: Remove duplicate import statement in GPT2.jsx

--- src/GPT2.jsx
+++ src/GPT2.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { Container, Form, FormControl, Button, Card } from "react-bootstrap";
 import { encodeURIComponent } from "querystring";
 import { Container, Form, FormControl, Button, Card } from "react-bootstrap";
 
